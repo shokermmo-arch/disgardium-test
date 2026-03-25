bbc89d4d7633d12e134d35028a104021a3ba2155
shokermmo-arch <shokermmo@gmail.com>
2026-03-25 17:16:38 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-// Имитация данных из Supabase
 const playerData = {
     gold: 962, silver: 99, copper: 96, diamonds: 458.25,
     hp: 338, max_hp: 663, mp: 327, max_mp: 327
 };
 
 function initGame() {
-    // Обновляем деньги
+    // Деньги
     document.getElementById('gold-val').innerText = playerData.gold;
     document.getElementById('silver-val').innerText = playerData.silver;
     document.getElementById('copper-val').innerText = playerData.copper;
     document.getElementById('diamonds-val').innerText = playerData.diamonds;
 
-    // Обновляем полоски
+    // Полоски
     const hpWidth = (playerData.hp / playerData.max_hp) * 100;
     document.getElementById('hp-bar').style.width = hpWidth + "%";
     
     const mpWidth = (playerData.mp / playerData.max_mp) * 100;
     document.getElementById('mp-bar').style.width = mpWidth + "%";
 
-    console.log("Интерфейс Векрона инициализирован.");
+    console.log("Интерфейс инициализирован.");
 }
 
 window.onload = initGame;
